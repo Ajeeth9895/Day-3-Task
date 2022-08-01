918dd8201b3ba38224f6746b3a259b040a47c27e
Ajeeth9895 <109730467+Ajeeth9895@users.noreply.github.com>
2022-08-01 16:25:13 +0530

    Add files via upload

---
+++ JSON-Resume.js
@@ -0,0 +1,14 @@
+let resume = [{
+    "name":"Ajeeth Kumar G",
+    "Age":26,
+    "SSLC":"Muhammadia High School-Umerabad",
+    "Diploma":"Rajagopal Polytechnical College-Vellore",
+    "college":"Nehru Institute Of Technology-Coimbatore",
+    "District":"Thirupatur",
+    "mobile":6382862595,
+    "mail":"ajeethkumar.gopal@gmail.com"
+}];
+
+
+
+
---
+++ JSON-loop.js
@@ -0,0 +1,25 @@
+
+// 1.For loop;
+
+let a = [{"name":"Ajeeth","Age":27}];
+
+for(let  i=0; i<a.length; i++){
+   console.log(a[i]);
+}
+
+// 2.For in loop;
+
+let b = [{"name":"Ajeeth","Age":27}];
+
+for (let x in b) {
+    console.log(b[x]);
+  }
+
+// 3.For off loop;
+
+let c = [{"name":"Ajeeth","Age":27}];
+
+for (let x of c) {
+    console.log(x);
+  }
+
